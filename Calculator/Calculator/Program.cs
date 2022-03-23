8eeccd321e43a27f0f1aff585da384153d2f5ccd
Artem Pershutkin <79787099+Artom-art@users.noreply.github.com>
2022-03-23 12:54:59 +0300

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Operators;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Incorrect number of parameters!");
+                return;
+            }
+
+            List<IOperator> operations = new List<IOperator>
+            {
+                new Addition(),
+                new Subtraction(),
+                new Multplication(),
+                new Division()
+            };
+
+            ICalculator calculator = new Calc(operations);
+            int result = calculator.Calculate(args[0]);
+
+            Console.WriteLine($"Result: {result}");
+        }
+    }
+}
