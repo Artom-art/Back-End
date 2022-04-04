28f75f81ff507d64e5c6192f830328873c08ab85
Artem Pershutkin <79787099+Artom-art@users.noreply.github.com>
2022-04-04 19:44:59 +0300

    Add files via upload

---
+++ ScrumBoard/ScrumBoardLibrary/IBoard.cs
@@ -0,0 +1,9 @@
+﻿namespace ScrumBoardLibrary
+{
+    public interface IBoard
+    {
+        void AddColumn(string columnName);
+        void AddTask(ITask task);
+        void MoveTask(IColumn columnFrom, IColumn columnTo, ITask task);
+    }
+}
