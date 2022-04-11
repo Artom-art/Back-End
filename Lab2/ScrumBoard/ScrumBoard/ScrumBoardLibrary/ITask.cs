c32379413a4c7dc2c910fb8f046dd95e77c06d55
Artem Pershutkin <79787099+Artom-art@users.noreply.github.com>
2022-04-11 20:49:01 +0300

    Add files via upload

---
+++ Lab2/ScrumBoard/ScrumBoard/ScrumBoardLibrary/ITask.cs
@@ -0,0 +1,10 @@
+﻿namespace ScrumBoardLibrary
+{
+    public interface ITask
+    {
+        string Name { get; }
+        void ChangeName(string newName);
+        void ChangePriority(int newPriority);
+        void ChangeDescription(string newDescrition);
+    }
+}
